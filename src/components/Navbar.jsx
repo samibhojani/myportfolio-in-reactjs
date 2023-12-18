21e0a39b219b0860e51d2b5f44bf384a32d78ce3
Sami Bhojani <65404958+samibhojani@users.noreply.github.com>
2023-12-18 12:34:06 +0500

    navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-import { Link } from 'react-scroll' 
+import React, { useState } from 'react';
+import { Link } from 'react-scroll';
+import { AiOutlineClose } from 'react-icons/ai';
+import { HiOutlineMenuAlt1 } from 'react-icons/hi';
 
 
 const Navbar = () => {
-  return (
+    
+    const [menu, setMenu] = useState(false)
+
+    const handleChange = () => {
+        setMenu(!menu);
+    }
+
+    const closeMenu = () => {
+        setMenu(false);
+    }
+
+    return (
     <div className='fixed w-full z-10'>
         <div>
             <div className='flex flex-row justify-between p-5 md:px-32 px-5 bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
@@ -12,11 +25,12 @@ const Navbar = () => {
                 </div>
 
                 <nav className='hidden lg:flex flex-row items-center text-lg font-medium gap-8'>
-                    <Link   to='home'
+                <Link   
+                            to='home'
                             spy={true}
                             smooth={true}
                             duration={500}
-                            className='cursor-pointer'
+                            className='hover:text-brightColors transition-all cursor-pointer'
                     >
                         Home
                     </Link>
@@ -25,7 +39,7 @@ const Navbar = () => {
                             spy={true}
                             smooth={true}
                             duration={500}
-                            className='cursor-pointer'
+                            className='hover:text-brightColors transition-all cursor-pointer'
                     >
                         About Me
                     </Link>
@@ -34,8 +48,8 @@ const Navbar = () => {
                             spy={true}
                             smooth={true}
                             duration={500}
-                            className='cursor-pointer'
-                    >
+                            className='hover:text-brightColors transition-all cursor-pointer'
+                >
                         Services
                     </Link>
                     <Link
@@ -43,7 +57,7 @@ const Navbar = () => {
                             spy={true}
                             smooth={true}
                             duration={500}
-                            className='cursor-pointer'
+                            className='hover:text-brightColors transition-all cursor-pointer'
                     >
                         Education
                     </Link>
@@ -52,7 +66,7 @@ const Navbar = () => {
                             spy={true}
                             smooth={true}
                             duration={500}
-                            className='cursor-pointer'
+                            className='hover:text-brightColors transition-all cursor-pointer'
                     >
                         Experience
                     </Link>
@@ -61,11 +75,83 @@ const Navbar = () => {
                             spy={true}
                             smooth={true}
                             duration={500}
-                            className='cursor-pointer'
+                            className='hover:text-brightColors transition-all cursor-pointer'
                     >
                         Portfolio
                     </Link>
                 </nav>
+
+
+                <div className='lg:hidden flex items-center'>
+                    {menu ? (
+                        <AiOutlineClose size={28} onClick={handleChange} />
+                    ): (
+                        <HiOutlineMenuAlt1 size={28} onClick={handleChange} />
+                    )
+                    } 
+                </div>
+            </div>
+            <div className={`${ menu ? "translate-x-0": "-translate-x-full"} lg-hidden flex flex-col absolute bg-black text-white left-0 top-20 font-semibold text-2xl text-center z-10 pt-8 gap-8 pb-4 w-full h-fit transition-transform duration-300`}>
+                    <Link   
+                            to='home'
+                            spy={true}
+                            smooth={true}
+                            duration={500}
+                            className='hover:text-brightColors transition-all cursor-pointer'
+                            onClick={closeMenu}
+                    >
+                        Home
+                    </Link>
+                    <Link
+                            to='about'
+                            spy={true}
+                            smooth={true}
+                            duration={500}
+                            className='hover:text-brightColors transition-all cursor-pointer'
+                            onClick={closeMenu}
+                    >
+                        About Me
+                    </Link>
+                    <Link
+                            to='services'
+                            spy={true}
+                            smooth={true}
+                            duration={500}
+                            className='hover:text-brightColors transition-all cursor-pointer'
+                            onClick={closeMenu}
+                >
+                        Services
+                    </Link>
+                    <Link
+                            to='education'
+                            spy={true}
+                            smooth={true}
+                            duration={500}
+                            className='hover:text-brightColors transition-all cursor-pointer'
+                            onClick={closeMenu}
+                    >
+                        Education
+                    </Link>
+                    <Link
+                            to='experience'
+                            spy={true}
+                            smooth={true}
+                            duration={500}
+                            className='hover:text-brightColors transition-all cursor-pointer'
+                            onClick={closeMenu}
+                    >
+                        Experience
+                    </Link>
+                    <Link
+                            to='portfolio'
+                            spy={true}
+                            smooth={true}
+                            duration={500}
+                            className='hover:text-brightColors transition-all cursor-pointer'
+                            onClick={closeMenu}
+                    >
+                        Portfolio
+                    </Link>
             </div>
         </div>
     </div>
